24fac6699adcf287f405abfe5955b93814533567
Arup Bhowmcik <arupbhowmick007@gmail.com>
2022-08-17 14:52:22 +0530

    Commiting complete - django app - (containerless)

--- web_project/urls.py
+++ web_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", include("hello.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+# To use below, first run `python manage.py collectstatic` everytime you update any static file.
+# urlpatterns += staticfiles_urlpatterns()
